ca88b50ca9fbf34c2f5944bb32af093ec142cc49
CC-Daniels <71735953+CC-Daniels@users.noreply.github.com>
2021-04-04 17:46:52 -0500

    Reupdated funtions in JS and rearranged function of displayforecast

--- src/app.js
+++ src/app.js
@@ -31,35 +31,39 @@ function formatDate(date) {
  function formatDay(currentTime) {
   let date = new Date(currentTime * 1000);
   let day = date.getDay();
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
   return days(day);
 }
 
-function displayForecast(response) {
-  let forecastElement = document.querySelector("#forecast");
-  console.log(response.data.daily);
-  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+ function displayForecast(response) {
+ let forecast = response.data.daily;
+
+ let forecastElement = document.querySelector("#forecast"); 
+  
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day, index) {
-    let forecastDay = response.data.daily[index];
+  forecast.forEach(function (forecastDay, index) {
+    
+    if (index < 6) {
     forecastHTML =
       forecastHTML +
       `<div class="col-2">
-     <div class="weather-forecast-date">${day}</div>
-     <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="sun" width="37"/>
+     <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+     <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="sun" width="37"/>
      <div class="weather-forecast-temperatures">
-       <span class="weather-forecast-min">${Math.round(forecastDay.temp.min)}
+       <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}
          °
       </span>
-       <span class="weather-forecast-max">${Math.round(forecastDay.temp.max)}
+       <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}
          °
        </span>
    </div>
    </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
  function getForecast(coordinates) {
   console.log(coordinates);
