1a9cfb8291b848b663d12dc2b03552b439fbbc9c
CC-Daniels <71735953+CC-Daniels@users.noreply.github.com>
2021-04-03 23:00:38 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -38,8 +38,8 @@ function formatDate(date) {
     forecastHTML =
       forecastHTML +
       `<div class="col-2">
-     <div class="weather-forecast-date">${formatDay(getForecast.dt)}</div>
-     <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="sun" width="37"/>
+     <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+     <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon@2x.png" alt="" width="37" />
      <div class="weather-forecast-temperatures">
        <span class="weather-forecast-min">${Math.round(forecastDay.temp.min)}
          °
