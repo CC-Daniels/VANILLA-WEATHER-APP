7c021cee8a4bec3e3d74e87d8dfba0add8da2246
CC-Daniels <71735953+CC-Daniels@users.noreply.github.com>
2021-04-04 19:14:43 -0500

    apiURL was moved around in function of JS

--- src/app.js
+++ src/app.js
@@ -58,7 +58,7 @@ function formatDate(date) {
   let apiUrl =  `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apikey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
  }
-
+ let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
  function displayWeatherCondition(response) {
     document.querySelector("#city").innerHTML = response.data.name;
