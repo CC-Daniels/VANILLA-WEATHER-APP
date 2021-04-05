7a1f5ccf8063d0207b99fc090a752002a0ffb2f7
CC-Daniels <71735953+CC-Daniels@users.noreply.github.com>
2021-04-04 21:43:01 -0500

    Fixed iconElement in JS

--- src/app.js
+++ src/app.js
@@ -75,7 +75,7 @@ function getForecast(coordinates) {
 
    iconElement.setAttribute("src", 
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);      
-  iconElement.setAttribute("alt",response.data.weather[0].description);
+   iconElement.setAttribute("alt",response.data.weather[0].description);
 
   getForecast(response.data.coord);
     }
